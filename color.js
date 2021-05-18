1a8a444e82fa052fb4592bfca8efca4a167ff0eb
Jacob <66869348+TheAwesomeJacob@users.noreply.github.com>
2021-05-18 16:18:19 +0200

    Add files via upload

--- color.js
+++ color.js
@@ -7,9 +7,18 @@ console.log(colorNumber)
 console.log(colorScreen)
 
 
-document.querySelector('.action').addEventListener('change', (event) =>{
+document.querySelector('.action').addEventListener('input', (event) =>{
     document.querySelectorAll('.button-calc').forEach(p => {
         p.style.color = event.target.value;
     })
-    console.log(document.querySelector('.button-calc'));
 })
+
+document.querySelector('.number').addEventListener('input', (event) =>{
+    document.querySelectorAll('.button-calc:last-child').forEach(p => {
+        p.style.backgroundColor = event.target.value;
+    })
+})
+
+document.querySelector('.color-screen').addEventListener('input', (event) =>{
+    document.querySelector('.screen').style.backgroundColor = event.target.value;
+    })
